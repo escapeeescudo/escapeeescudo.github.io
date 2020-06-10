64b0618ada4ce354eada96516de113822bfcc714
Vibhor Chaudhary <vibhor@moveinsync.com>
2020-06-10 13:47:29 +0530

    Updates

---
+++ precache-manifest.721574d85e32a2e4817c06633111cafb.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "236a79820953b943073e4d5523cdde0b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "731c525ff8a1cb302a89",
+    "url": "/static/css/main.48d5fd3b.chunk.css"
+  },
+  {
+    "revision": "caab79e9387d8293d8a4",
+    "url": "/static/js/2.d9115052.chunk.js"
+  },
+  {
+    "revision": "4a1691fd44116c5f8c99a494e6966d73",
+    "url": "/static/js/2.d9115052.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "731c525ff8a1cb302a89",
+    "url": "/static/js/main.efe99289.chunk.js"
+  },
+  {
+    "revision": "d075d5ca806366893dd5",
+    "url": "/static/js/runtime-main.1072ffe1.js"
+  },
+  {
+    "revision": "1967bd6c78dc2cfd149b722f7efc5b6b",
+    "url": "/static/media/behance.1967bd6c.svg"
+  },
+  {
+    "revision": "e917de26ceadbd7c21104ab5577a5ed0",
+    "url": "/static/media/facebook.e917de26.svg"
+  },
+  {
+    "revision": "15bc5470aea6d2d05200dff45c14e92b",
+    "url": "/static/media/instagram.15bc5470.svg"
+  },
+  {
+    "revision": "14f7b6753cb58692c0da571c41b33210",
+    "url": "/static/media/linkedin.14f7b675.svg"
+  }
+]);
